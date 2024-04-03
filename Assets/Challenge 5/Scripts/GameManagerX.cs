5b0f1d1719bc27d4bdbaafa573d30e8da22ee7f4
benjaminlule <142627571+benjaminlule@users.noreply.github.com>
2024-04-03 12:42:17 -0700

    Unit 5 Challenge

--- Assets/Challenge 5/Scripts/GameManagerX.cs
+++ Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -21,18 +21,28 @@ public class GameManagerX : MonoBehaviour
     private float spaceBetweenSquares = 2.5f; 
     private float minValueX = -3.75f; //  x value of the center of the left-most square
     private float minValueY = -3.75f; //  y value of the center of the bottom-most square
-    
+    public TextMeshProUGUI timerText;
     // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
-    public void StartGame()
+    public void StartGame(int difficulty)
     {
-        spawnRate /= 5;
+        spawnRate /= difficulty;
         isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
         titleScreen.SetActive(false);
+        StartCoroutine(GameTimer());
     }
-
+    private IEnumerator GameTimer()
+    {
+        int timeLeft = 60;
+        while (timeLeft > 0)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+            timerText.text = "Time: " + timeLeft;
+        }
+    }
     // While game is active spawn a random target
     IEnumerator SpawnTarget()
     {
@@ -70,15 +80,16 @@ public class GameManagerX : MonoBehaviour
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "score";
+        scoreText.text = "Score : " + score;
     }
 
     // Stop game, bring up game over text and restart button
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
-        restartButton.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(true);
         isGameActive = false;
+            timerText.gameObject.SetActive(false);
     }
 
     // Restart game by reloading the scene
